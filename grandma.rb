ce5e67477474e2c9fb57e36475f3a6ac5faa1749
imperative-tuple-4883 <andrew.witmer445@gmail.com>
2020-05-16 03:18:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,15 @@
 
 
-def speak_to_grandma
+def speak_to_grandma(phrase)
   
-  if speak_to_grandma == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
-  
-  elsif speak_to_grandma == "I hear you GRANDMA!"
-    puts "HUH?! SPEAK UP SONNY!"
-  
-  else speak_to_grandma == "I HEAR YOU GRANDMA!".upcase
-    puts "NO, NOT SINCE 1938!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    
+  elsif phrase != phrase.upcase
+     return "HUH?! SPEAK UP, SONNY!"
+     
+  else
+    return "NO, NOT SINCE 1938!"
     
   end
   
